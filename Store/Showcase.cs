b72762164eb3dc096b65a8c33b34b8eacfb6e6cf
Katarina <katarinaslaursen@gmail.com>
2020-02-05 09:49:05 +0100

    Worked on adding and removing games

--- Store/Showcase.cs
+++ Store/Showcase.cs
@@ -9,37 +9,55 @@ namespace Store
     class Showcase
     {
         Game[] games;
-        
+        bool bought = false;
+        int size;
+        int[] numberOfGames;
 
         public Showcase(int size)
         {
             games = new Game[size];
-            
+            this.size = size;
 
         }
 
         public void AddGame()
         {
-            /*for (int)
+            //For every slot we want to put in a game
+            for (int i=0; i<size; i++)
             {
+                Game g[i] = new Game(??);
+            }
 
-            }*/
+            //or?
 
+            numberOfGames = new int [size];
+
+            foreach (int i in numberOfGames)
+            {
+                Game g[i] = new Game (??); 
+            }
+            
+            //Check if alreadyBought
             
         }
 
         public void RemoveGame()
         {
-            bool alreadyBought;
-            if (alreadyBought = true)
-            {
+            bool bought = true;
 
-            }
+            //Remove specific number game - find the number it has, then remove it, and add another
+            AddGame();
         }
 
         public void Show()
         {
-
+            bool alreadyBought = false;
+            if (alreadyBought = true)
+            {
+                RemoveGame();
+                alreadyBought = false;
+                
+            }
         }
     }
 }
